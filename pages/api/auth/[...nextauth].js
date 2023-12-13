358554681737b43e321bd46e0b3bd061586c63d3
mahardikadwi <dwimahardika08@gmail.com>
2023-12-13 21:30:00 +0700

    minor changes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,20 +1,15 @@
-import NextAuth from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { MongoDBAdapter } from '@auth/mongodb-adapter';
-import clientPromise from '@/lib/mongodb';
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { MongoDBAdapter } from '@auth/mongodb-adapter'
+import clientPromise  from '@/lib/mongodb'
 
 export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      scope: 'profile email',
+      clientSecret: process.env.GOOGLE_SECRET
     }),
   ],
+  adapter: MongoDBAdapter(clientPromise),
   secret: process.env.NEXTAUTH_SECRET,
-  adapter: MongoDBAdapter({
-    client: await clientPromise,
-    dbName: 'next-ecommerce', // Replace with your actual database name
-  }),
-  database: process.env.MONGODB_URI,
-});
+})
